4c9dbea44a3d38c286835e43336b68f03f8f7a4a
siwakasen <siwakasen@gmail.com>
2026-02-02 22:15:16 +0800

    feat: structs, methods, & interfaces

---
+++ shapes/shapes.go
@@ -0,0 +1,6 @@
+// Package shapes is package that provides functions for stucts, methods, interfaces  test cases
+package shapes
+
+func Perimeter(width float64, height float64) float64 {
+	return 0
+}
