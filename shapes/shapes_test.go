5efc8adb56eceb7e6325e3b021f0ba5e6b45e8c8
siwakasen <siwakasen@gmail.com>
2026-02-03 21:16:54 +0800

    refactor: naming test case of test area

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -14,19 +14,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTest := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{"Rectangle", Rectangle{12.0, 6.0}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTest {
-		got := tt.shape.Area()
-		want := tt.want
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			hasArea := tt.hasArea
+			if got != hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, hasArea)
+			}
+		})
 	}
 }
