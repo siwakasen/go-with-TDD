b01e358d8c9dd11fc09b285e747beace6526b182
siwakasen <siwakasen@gmail.com>
2026-02-03 21:09:23 +0800

    feat: adding triangle and comments for interface

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -30,6 +30,16 @@ func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
 }
 
+// Shape interface with Area
 type Shape interface {
 	Area() float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
 }
