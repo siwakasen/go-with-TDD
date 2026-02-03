9eb596dc410a2a9b53e50dd5be084e8e6e294a6b
siwakasen <siwakasen@gmail.com>
2026-02-03 20:58:15 +0800

    feat: adding methods for shape

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,9 +1,25 @@
 // Package shapes is package that provides functions for stucts, methods, interfaces  test cases
 package shapes
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// Area method for rectangle: rectangle.Area()
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// Area method for circle: circle.Area()
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
@@ -12,4 +28,8 @@ func Perimeter(rectangle Rectangle) float64 {
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+type Shape interface {
+	Area() float64
 }
