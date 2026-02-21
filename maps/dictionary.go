ce19c848278d2ea3899845cf9587fe8a12bc9053
siwakasen <siwakasen@gmail.com>
2026-02-21 18:04:49 +0800

    fix: update maps test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,18 @@
 // Package maps
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 // Search method for Dictionary: dictionary.Search()
 func (d Dictionary) Search(word string) (string, error) {
@@ -17,6 +24,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
