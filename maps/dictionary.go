7e97b4bfd09156a3d9784c1bd054b4c91540a473
siwakasen <siwakasen@gmail.com>
2026-02-25 15:48:21 +0800

    feat: add dictionary struct and type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,1 +1,8 @@
+// Package maps
 package maps
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,1 +1,22 @@
 package maps
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	got := dictionary.Search("test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
