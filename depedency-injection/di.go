26e83a4abb9b412568bb1dd05662398418443869
siwakasen <siwakasen@gmail.com>
2026-02-25 21:13:39 +0800

    feat: add function to test di

--- depedency-injection/di.go
+++ depedency-injection/di.go
@@ -1,2 +1,11 @@
 // Package depedencyinjection
 package depedencyinjection
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
