2fdc8eb53b8d7d65f1ece1378436e5a8f572564f
siwakasen <siwakasen@gmail.com>
2026-03-06 22:32:19 +0700

    fix: renaming package import

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/siwakasen/go-with-TDD/variadic"
+	vrdc "github.com/siwakasen/go-with-TDD/variadic"
 )
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
 
-	fmt.Printf("%d", variadic.Calculate(numbers...))
+	fmt.Printf("%q", vrdc.Calculate(numbers...))
 }
