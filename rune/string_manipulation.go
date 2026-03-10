55b677dec5660fc342fdfa7c7fd17fbbd243668f
siwakasen <siwakasen@gmail.com>
2026-03-10 15:10:45 +0800

    feat: string checker without using unicode

--- rune/string_manipulation.go
+++ rune/string_manipulation.go
@@ -1,6 +1,20 @@
 // Package stringmanipulation
 package stringmanipulation
 
+import "strings"
+
+func isLetter(c byte) bool {
+	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'
+}
+
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func isSymbol(c byte) bool {
+	return !isLetter(c) && !isDigit(c) && c != ' ' && c != '\n' && c != '\t'
+}
+
 func reverseString(s string) string {
 	runes := []rune(s)
 
@@ -8,4 +22,15 @@ func reverseString(s string) string {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
+}
+
+func palindromOnlyLetter(s string) string {
+	var letter strings.Builder
+
+	for i := 0; i < len(s); i++ {
+		if isLetter(s[i]) {
+			letter.WriteByte(s[i])
+		}
+	}
+	return reverseString(strings.ToLower(letter.String()))
 }
--- rune/string_manipulation_test.go
+++ rune/string_manipulation_test.go
@@ -12,7 +12,32 @@ func TestReverseString(t *testing.T) {
 
 	got := reverseString(s)
 
-	if want != got {
-		t.Errorf("got %s want %s", got, want)
+	checkString(t, got, want)
+}
+
+func TestIsPalindrom(t *testing.T) {
+	t.Run("palimdrom normal letter", func(t *testing.T) {
+		original := "alla"
+		want := original
+		got := reverseString(original)
+
+		checkString(t, got, want)
+	})
+
+	t.Run("palindrom with filtering non-letter", func(t *testing.T) {
+		original := "al1l.A9#"
+
+		want := "alla"
+		got := palindromOnlyLetter(original)
+
+		checkString(t, got, want)
+	})
+}
+
+func checkString(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
