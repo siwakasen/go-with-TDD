22271c4493aef97b4fee91b733b230e275ed3989
siwakasen <siwakasen@gmail.com>
2026-03-10 15:27:07 +0800

    feat: add string checker for digit and symbols

--- rune/string_manipulation_test.go
+++ rune/string_manipulation_test.go
@@ -2,6 +2,7 @@
 package stringmanipulation
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -12,7 +13,7 @@ func TestReverseString(t *testing.T) {
 
 	got := reverseString(s)
 
-	checkString(t, got, want)
+	checkTwoSrings(t, got, want)
 }
 
 func TestIsPalindrom(t *testing.T) {
@@ -21,7 +22,7 @@ func TestIsPalindrom(t *testing.T) {
 		want := original
 		got := reverseString(original)
 
-		checkString(t, got, want)
+		checkTwoSrings(t, got, want)
 	})
 
 	t.Run("palindrom with filtering non-letter", func(t *testing.T) {
@@ -30,11 +31,50 @@ func TestIsPalindrom(t *testing.T) {
 		want := "alla"
 		got := palindromOnlyLetter(original)
 
-		checkString(t, got, want)
+		checkTwoSrings(t, got, want)
 	})
 }
 
-func checkString(t testing.TB, got, want string) {
+func TestStringChecker(t *testing.T) {
+	t.Run("is digit?", func(t *testing.T) {
+		s := "1"
+		want := true
+		got := isDigit(s[0])
+
+		checkTwoBoolean(t, got, want)
+	})
+
+	t.Run("is Symbol", func(t *testing.T) {
+		s := "%.*#;,"
+		want := []bool{true, true, true, true, true, true}
+
+		got := func() []bool {
+			var result []bool
+
+			for i := 0; i < len(s); i++ {
+				if isSymbol(s[i]) {
+					result = append(result, true)
+				} else {
+					result = append(result, true)
+				}
+			}
+			return result
+		}()
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
+}
+
+func checkTwoBoolean(t testing.TB, got, want bool) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+}
+
+func checkTwoSrings(t testing.TB, got, want string) {
 	t.Helper()
 
 	if got != want {
