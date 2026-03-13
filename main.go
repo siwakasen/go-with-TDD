7a9b82aa5cf9fb1ee92dfc6af571b701bcd0924a
siwakasen <siwakasen@gmail.com>
2026-03-13 22:42:48 +0800

    feat: make real http server to have in memory player store

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"log"
 	"net/http"
 
-	httpserver "github.com/siwakasen/go-with-TDD/httpserver"
+	"github.com/siwakasen/go-with-TDD/httpserver"
 	vrdc "github.com/siwakasen/go-with-TDD/variadic"
 )
+
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
@@ -16,6 +22,6 @@ func main() {
 	fmt.Printf("%d\n", vrdc.Calculate(numbers...))
 
 	// httpserver
-	handler := http.HandlerFunc(httpserver.PlayerServer)
-	log.Fatal(http.ListenAndServe(":5001", handler))
+	server := &httpserver.PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5001", server))
 }
