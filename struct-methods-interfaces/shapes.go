c7a8a16b50f0aba4837a0d53962edf6cc6e57e5c
siwakasen <siwakasen@gmail.com>
2026-03-21 19:24:11 +0800

    fix: move shape interface line

--- struct-methods-interfaces/shapes.go
+++ struct-methods-interfaces/shapes.go
@@ -16,6 +16,11 @@ type Circle struct {
 	Radius float64
 }
 
+// Shape interface with Area
+type Shape interface {
+	Area() float64
+}
+
 // Area method for rectangle: r.Area()
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
@@ -29,11 +34,6 @@ func (c Circle) Area() float64 {
 // Area method for triangle: t.Area()
 func (t Triangle) Area() float64 {
 	return 0.5 * (t.Base * t.Height)
-}
-
-// Shape interface with Area
-type Shape interface {
-	Area() float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
