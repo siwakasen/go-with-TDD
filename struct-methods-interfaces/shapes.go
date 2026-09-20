aaed09a4ea789a75eb812f4a88eef13ae10eaec9
siwakasen <siwakasen@gmail.com>
2026-09-20 15:15:30 +0700

    fix(method): use pointer to decrease struct creation

--- struct-methods-interfaces/shapes.go
+++ struct-methods-interfaces/shapes.go
@@ -22,17 +22,17 @@ type Shape interface {
 }
 
 // Area method for rectangle: r.Area()
-func (r Rectangle) Area() float64 {
+func (r *Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
 
 // Area method for circle: c.Area()
-func (c Circle) Area() float64 {
+func (c *Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
 // Area method for triangle: t.Area()
-func (t Triangle) Area() float64 {
+func (t *Triangle) Area() float64 {
 	return 0.5 * (t.Base * t.Height)
 }
 
--- struct-methods-interfaces/shapes_test.go
+++ struct-methods-interfaces/shapes_test.go
@@ -18,9 +18,9 @@ func TestArea(t *testing.T) {
 		shape   Shape
 		hasArea float64
 	}{
-		{"Rectangle", Rectangle{12.0, 6.0}, 72.0},
-		{"Circle", Circle{10}, 314.1592653589793},
-		{"Triangle", Triangle{12, 6}, 36.0},
+		{"Rectangle", &Rectangle{12.0, 6.0}, 72.0},
+		{"Circle", &Circle{10}, 314.1592653589793},
+		{"Triangle", &Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTest {
