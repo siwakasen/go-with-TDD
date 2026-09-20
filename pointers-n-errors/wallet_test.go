05a546b1a51cb05cfbf77b2ebf6040c652f434e9
siwakasen <siwakasen@gmail.com>
2026-09-20 22:08:37 +0700

    fix(pointer): change assertBalance parameter to pointer to keep wallet address same

--- pointers-n-errors/wallet_test.go
+++ pointers-n-errors/wallet_test.go
@@ -1,14 +1,18 @@
 package wallet
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		want := Bitcoin(10)
+		fmt.Printf("address of balance in test: %p \n", &wallet.balance)
 
-		assertBalance(t, wallet, want)
+		assertBalance(t, &wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
@@ -16,18 +20,18 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(10))
 
 		assertNoError(t, err)
-		assertBalance(t, wallet, Bitcoin(10))
+		assertBalance(t, &wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(100))
 		assertError(t, err, "cannot withdraw, insufficient funds")
-		assertBalance(t, wallet, Bitcoin(20))
+		assertBalance(t, &wallet, Bitcoin(20))
 	})
 }
 
-func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+func assertBalance(t testing.TB, wallet *Wallet, want Bitcoin) {
 	t.Helper()
 
 	got := wallet.Balance()
